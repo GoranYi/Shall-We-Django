04d4c7834554292767f87aeaede8d6a3f023a713
Goran-Yi <yigoran1248@gmail.com>
2024-03-17 16:04:19 +0900

    models, database, migration, admin site

--- shall/apps/models.py
+++ shall/apps/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Wiki(models.Model):
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)    # auto_now_add는 최초 저장 시에만 적용, ineditable
+    updated_at = models.DateTimeField(auto_now=True)        # auto_now는 객체가 저장될 때마다 적용
